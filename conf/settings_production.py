44c7ea61012d033d45024e4cf608b23af2512186
唐一 <tjm_1990@126.com>
2017-03-12 19:45:40 +0800

    change test & prod database engine to sqlite

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -10,11 +10,7 @@ from settings import APP_ID
 # ===============================================================================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+        'ENGINE': 'django.db.backends.sqlite3',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
     },
 }
